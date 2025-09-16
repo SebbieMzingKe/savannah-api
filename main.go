f6c1f8a2e303d540901268d6fe6874494172a293
SebbieMzingKe <sebbievilar2@gmail.com>
2025-09-16 15:59:15 +0300

    chore: modify entry point.

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
-func init()  {
-	
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// Connect to database
+	var err error
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = "host=localhost user=savannah password=savannah dbname=savannah port=5432 sslmode=disable"
+	}
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = db.AutoMigrate()
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
 }
 
 func main() {
-	
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
